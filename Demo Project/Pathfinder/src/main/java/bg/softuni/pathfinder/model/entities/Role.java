cf93fd4ffbfa8823d1dc41918583a3324d465de1
Doichin251002 <dodi_hristov@abv.bg>
2024-06-10 10:46:04 +0300

    Create Data Layer and transfer database info from Model to View Layer

---
+++ Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/Role.java
@@ -0,0 +1,33 @@
+package bg.softuni.pathfinder.model.entities;
+
+import bg.softuni.pathfinder.model.enums.UserRole;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole name;
+
+    public Role() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public UserRole getName() {
+        return name;
+    }
+
+    public void setName(UserRole name) {
+        this.name = name;
+    }
+}
