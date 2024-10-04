b086ac69b4bf7340eae0e4afa0b45ad3614a3da6
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:17:02 +0300

    Create offer controller

---
+++ Demo Project/mobilele/mobilele/src/main/java/bg/softuni/mobilele/web/OfferController.java
@@ -0,0 +1,54 @@
+package bg.softuni.mobilele.web;
+
+import bg.softuni.mobilele.model.dtos.AddOfferDTO;
+import bg.softuni.mobilele.model.enums.EngineTypeEnum;
+import bg.softuni.mobilele.service.OfferService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/offers")
+public class OfferController {
+
+    private OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping("/add")
+    public String addOffer(Model model) {
+
+        if (!model.containsAttribute("addOfferDTO")) {
+            model.addAttribute("addOfferDTO", AddOfferDTO.empty());
+        }
+        model.addAttribute("allEngineTypes", EngineTypeEnum.values());
+
+        return "offer-add";
+    }
+
+    @PostMapping("/add")
+    public String createOffer(@Valid AddOfferDTO addOfferDTO,
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("addOfferDTO", addOfferDTO);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.addOfferDTO",
+                    bindingResult);
+
+            return "redirect:/offers/add";
+        }
+
+        long offerId = this.offerService.createOrder(addOfferDTO);
+
+        return "redirect:/offers/" + offerId;
+    }
+}
