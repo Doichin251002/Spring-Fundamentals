e65c688d3069e9729f084d5a1bbf645ce2960d7f
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:13:18 +0300

    Create offer entity

---
+++ Demo Project/mobilele/mobilele/src/main/java/bg/softuni/mobilele/model/entities/OfferEntity.java
@@ -0,0 +1,39 @@
+package bg.softuni.mobilele.model.entities;
+
+import bg.softuni.mobilele.model.enums.EngineTypeEnum;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "offers")
+public class OfferEntity extends BaseEntity {
+    private String description;
+
+    private Integer mileage;
+
+    @Enumerated(EnumType.STRING)
+    private EngineTypeEnum engine;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EngineTypeEnum getEngine() {
+        return engine;
+    }
+
+    public void setEngine(EngineTypeEnum engine) {
+        this.engine = engine;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+}
