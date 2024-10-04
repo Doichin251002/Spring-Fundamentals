453e778044563bcbc934a9488bd48d6d6632ebc2
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:57:26 +0300

    Add annotations for noArgsConstr, Getter and Setter

--- Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/Route.java
+++ Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/Route.java
@@ -2,6 +2,9 @@ package bg.softuni.pathfinder.model.entities;
 
 import bg.softuni.pathfinder.model.enums.Level;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -10,6 +13,8 @@ import java.util.Set;
 
 @Entity
 @Table(name = "routes")
+@Getter
+@Setter
 public class Route {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,85 +53,5 @@ public class Route {
         this.comments = new ArrayList<>();
         this.pictures = new HashSet<>();
         this.categories = new HashSet<>();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getGPXCoordinates() {
-        return GPXCoordinates;
-    }
-
-    public void setGPXCoordinates(String GPXCoordinates) {
-        this.GPXCoordinates = GPXCoordinates;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
-    public String getVideoURL() {
-        return videoURL;
-    }
-
-    public void setVideoURL(String videoURL) {
-        this.videoURL = videoURL;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public Set<Picture> getPictures() {
-        return pictures;
-    }
-
-    public void setPictures(Set<Picture> pictures) {
-        this.pictures = pictures;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
     }
 }
