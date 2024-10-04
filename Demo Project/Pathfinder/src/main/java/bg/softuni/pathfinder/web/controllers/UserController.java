51f566cc90e1792bf358f1985ad0de49a605c684
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:57:35 +0300

    Add annotations for noArgsConstr, Getter and Setter

--- Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/web/controllers/UserController.java
+++ Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/web/controllers/UserController.java
@@ -7,19 +7,21 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("users")
 public class UserController {
 
-    @GetMapping("/users/register")
+    @GetMapping("/register")
     public String viewRegister(Model model) {
         model.addAttribute("registerUser", new UserRegisterDTO());
 
         return "register";
     }
 
-    @PostMapping("/users/register")
+    @PostMapping("/register")
     public String register(@Valid UserRegisterDTO user,
                            BindingResult bindingResult,
                            RedirectAttributes redirectAttributes) {
@@ -32,7 +34,7 @@ public class UserController {
         return "redirect:/users/login";
     }
 
-    @GetMapping("/users/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
