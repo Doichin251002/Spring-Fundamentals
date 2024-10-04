a4225bbf016fb248d1b8eafe6226abe5849bfc50
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:14:16 +0300

    Detele empty dto for user registration

--- Demo Project/mobilele/mobilele/src/main/java/bg/softuni/mobilele/model/UserRegistrationDTO.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.mobilele.model;
-
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String username,
-                                  String password) {
-}
