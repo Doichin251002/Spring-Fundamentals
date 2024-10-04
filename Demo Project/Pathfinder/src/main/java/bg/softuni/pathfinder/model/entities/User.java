d23e3deb135b81fcacf3b603cd6a8b2f7b541d94
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:57:30 +0300

    Add annotations for noArgsConstr, Getter and Setter

--- Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/User.java
+++ Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/User.java
@@ -2,12 +2,17 @@ package bg.softuni.pathfinder.model.entities;
 
 import bg.softuni.pathfinder.model.enums.Level;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,69 +41,5 @@ public class User {
 
     public User() {
         this.roles = new HashSet<>();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
     }
 }
