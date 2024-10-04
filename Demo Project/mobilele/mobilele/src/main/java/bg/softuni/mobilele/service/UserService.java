527d933bf8eed683fbe31b867db8f8504a2dc9f9
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:16:17 +0300

    Create user service

--- Demo Project/mobilele/mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
+++ Demo Project/mobilele/mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -1,7 +1,10 @@
 package bg.softuni.mobilele.service;
 
-import bg.softuni.mobilele.model.UserRegistrationDTO;
+import bg.softuni.mobilele.model.dtos.UserLoginDTO;
+import bg.softuni.mobilele.model.dtos.UserRegistrationDTO;
 
 public interface UserService {
     void registerUser(UserRegistrationDTO userRegistration);
+
+    boolean login(UserLoginDTO userLogin);
 }
