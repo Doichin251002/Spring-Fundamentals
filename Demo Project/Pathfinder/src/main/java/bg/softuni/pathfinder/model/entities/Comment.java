35a97abdf2a6f737af343131c352ef53ebc9f1e6
Doichin251002 <dodi_hristov@abv.bg>
2024-10-04 18:57:09 +0300

    Add annotations for noArgsConstr, Getter and Setter

--- Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/Comment.java
+++ Demo Project/Pathfinder/src/main/java/bg/softuni/pathfinder/model/entities/Comment.java
@@ -1,11 +1,17 @@
 package bg.softuni.pathfinder.model.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
 @Entity
 @Table(name = "comments")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,55 +32,4 @@ public class Comment {
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Route route;
-
-    public Comment() {}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public boolean isApproved() {
-        return isApproved;
-    }
-
-    public void setApproved(boolean isApproved) {
-        this.isApproved = isApproved;
-    }
-
-
-    public Instant getCreated() {
-        return created;
-    }
-
-    public void setCreated(Instant created) {
-        this.created = created;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public Route getRoute() {
-        return route;
-    }
-
-    public void setRoute(Route route) {
-        this.route = route;
-    }
 }
